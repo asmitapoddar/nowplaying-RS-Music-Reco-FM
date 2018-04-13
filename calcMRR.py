669088e5a5de3bd2d63b2c2b9ecd242c8e78fb36
Asmita Poddar <asmita_poddar@yahoo.co.in>
2018-04-14 01:12:39 +0530

    Update calcMRR.py

--- calcMRR.py
+++ calcMRR.py
@@ -3,7 +3,7 @@ def MRR(preds):
 	import numpy as np
 	import math
 	
-	df = pd.read_csv('test_100_10K_sorted.txt', sep = '\t', header = None)
+	df = pd.read_csv('test_final_POP_RND_sorted.txt', sep = '\t', header = None)
 	df.columns = ['coordinates','instrumentalness', 'liveness','speechiness', 'danceability','valence','loudness', 'tempo','acousticness','energy','mode', 'key', 'artist_id','place','geo','tweet_lang','track_id','created_at', 'lang', 'time_zone','entities', 'user_id', 'id', 'rating']
 
 	unique_users = df['user_id'].value_counts()
